adff409dfc4ea29d6cad6432b6a3bfcab18e81f9
Danny <ngo.danny11@gmail.com>
2019-06-11 00:13:38 -0700

    created class Roomba along with createBoard and hooveringPosition functions

--- src/index.js
+++ src/index.js
@@ -7,3 +7,33 @@ const start = inputData.slice(1, 2).toString().split(' ');
 const dirtPatches = inputData.slice(2, inputData.length - 1);
 const directions = inputData[inputData.length - 1].split('');
 
+class Roomba {
+    constructor(rows = 5, columns = 5) {
+        this.board = Roomba.createBoard(rows, columns);
+        this.rowsLength = rows;
+        this.columnsLength = columns;
+        this.dirtClean = 0;
+    }
+    //initialize board
+    static createBoard(rows, columns) {
+        let board = [];
+        for (let i = 0; i < rows; i++) {
+            let row = [];
+            for (let j = 0; j < columns; j++) {
+                row.push(' ');
+            }
+            board.push(row);
+        }
+        return board;
+    }
+    //tracking movements
+    hooveringPosition(xPos, yPos) {
+        this.hooverPos = {
+            x: xPos,
+            y: yPos
+        }
+        this.board[yPos][xPos] = 'hoover';
+    }
+
+
+}
