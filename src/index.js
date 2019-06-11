3bce69ac4cb5f6e9e71b7ef632ab0d79e39033f7
Danny <ngo.danny11@gmail.com>
2019-06-10 23:51:59 -0700

    parse input.txt file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 const fs = require('fs');
+
+// Parsing input.txt file line by line
+const inputData = fs.readFileSync('input.txt', 'utf-8').split('\n');
+const boardSize = inputData.slice(0, 1).toString().split(' ');
+const start = inputData.slice(1, 2).toString().split(' ');
+const dirtPatches = inputData.slice(2, inputData.length - 1);
+const directions = inputData[inputData.length - 1].split('');
+
